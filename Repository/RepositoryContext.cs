cf1ff5940b333feeb86d970fa8c1cd61a30dd726
sabbapxakadze <xdxsabuka@gmail.com>
2025-04-24 16:54:38 +0400

    InitialConfiguration

---
+++ Repository/RepositoryContext.cs
@@ -0,0 +1,20 @@
+﻿using Entities;
+using Microsoft.EntityFrameworkCore;
+using Repository.Configuration;
+
+namespace Repository;
+
+public class RepositoryContext : DbContext
+{
+    public RepositoryContext(DbContextOptions options) : base(options)
+    {
+        
+    }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfiguration(new CompanyConfiguration());
+        modelBuilder.ApplyConfiguration(new EmployeeConfiguration());
+    }
+    DbSet<Company>? Companies { get; set; }
+    DbSet<Employee>? Employees { get; set; }
+}
